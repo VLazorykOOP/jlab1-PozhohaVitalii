dfa1e8d025bdb10d650b4499c94ab6165b94c3b8
PozhohaVitalii <112852134+PozhohaVitalii@users.noreply.github.com>
2023-02-14 12:25:34 +0200

    3/4

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,46 @@
+import java.util.Scanner;
+
 public class Main {
+    static Task_1 LOBJ1 = new Task_1();
+    static Task_2 LOBJ2 = new Task_2();
+    static Task_3 LOBJ3 = new Task_3();
+    static Task_4 LOBJ4 = new Task_4();
+    public static void task_1(){
+        LOBJ1.modeAsk();
+        LOBJ1.signAsk();
+        LOBJ1.exerciseCalculation();
+        LOBJ1.showResults();
+    }
+    public static void task_2(){
+        LOBJ2.inputManager("OO");
+        LOBJ2.calculations();
+        LOBJ2.showResult();
+
+    }
+    public static void task_3(){
+        LOBJ3.setMatrix();
+        LOBJ3.findMin();
+    }
+    public static void task_4(){
+
+    }
 
     public static void main(String[] args) {
+        Scanner pop = new Scanner(System.in);
+        System.out.println("Choice task to test (1,2,3,4)");
+        switch (pop.nextInt()) {
+            case (1):
+                task_1();
+            case (2):
+                task_2();
+            case (3):
+                task_3();
+            case (4):
+                task_4();
+        }
 	// write your code here
         System.out.println("Lab 1 Java");
     }
+
+
 }
